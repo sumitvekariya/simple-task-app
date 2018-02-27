65df0a7a6e8a5fbdb1b1164957ca3de089b6b480
sumitvekariya <sumitvekariya7@gmail.com>
2018-02-27 19:47:49 +0530

    First Commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  goals: any;
+  id: any = -1;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) {
+    this.route.params.subscribe(res => this.id = res.id);
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
+  }
+
+  
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations'
+import { animation } from '@angular/animations/src/animation_metadata';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('goals',[
+      transition('* => *',[
+        query(':enter',style({opacity: 0}),{optional: true}),
+
+        query(':enter',stagger('300ms',[
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))]),{optional: true}),
+        
+          query(':leave',stagger('300ms',[
+            animate('.6s ease-out', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+              style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+            ]))]),{optional: true})
+      ])
+    ])
+
+  ]
+})
+
+export class HomeComponent implements OnInit {
+
+  itemCount: number = 4;
+  btnText: string = 'Add an Item';
+  goalText: string = 'My First life Goal';
+  goals = ['Buy TT Bat', 'Buy Headphones', 'Buy Good Camera Mobile', 'Gift Mom a Mobile'];
+
+  constructor(private _data : DataService, private router : Router) {
+
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals=res);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+  addItem(){
+    if(this.goalText!=''){
+      this.goals.push(this.goalText);
+      this.goalText='';
+      this.itemCount=this.goals.length;
+      this._data.changeGoal(this.goals);
+    }
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+  viewItem(i){
+    this.router.navigate(['about/'+i]);
+  }
+}
